c9b77bfd1a892f2ac847ea1d7ef5be8edb32cf50
yatsu3 <yatsutako3gmail.com>
2025-02-22 12:55:45 +0900

    firebaseを使った認証機能の実装

---
+++ src/Header.tsx
@@ -0,0 +1,39 @@
+import { AppBar, Box, Button, IconButton, Typography } from "@mui/material";
+import Toolbar from "@mui/material/Toolbar/Toolbar";
+import React, { useContext, useEffect } from "react";
+import { useNavigate } from 'react-router-dom';
+import { getAuth, signOut } from "firebase/auth";
+
+const Header: React.FC = () => {
+    const navigate = useNavigate();
+
+    const logout = async () => {
+      try {
+        const auth = getAuth();
+        await signOut(auth);
+        alert("ログアウトしました。");
+        navigate("/");
+        } catch(e) {
+        alert("ログアウトができませんでした。");
+      }
+    }
+
+    return (
+        <>
+        <AppBar position="static">
+      <Toolbar>
+        <IconButton edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
+        </IconButton>
+        <Typography variant="h6" sx={{ flexGrow: 1 }}>
+          ヘッダー
+        </Typography>
+            <Button color="inherit" onClick={() => {navigate('/test');}}>テスト</Button>
+            <Button color="inherit" onClick={() => {navigate('/question-list');}}>問題一覧</Button>
+            <Button color="inherit" onClick={() => logout()}>ログアウト</Button>
+      </Toolbar>
+    </AppBar>
+    </>
+    );
+}
+
+export default Header;
