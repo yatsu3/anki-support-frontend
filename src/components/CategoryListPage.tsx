f25da8aee5b1ae2c72e16197ab03c91a19ca48e0
yatsu3 <yatsutako3gmail.com>
2025-05-02 11:18:30 +0900

    開発用と本番用でAPI切り替えの検証

--- src/components/CategoryListPage.tsx
+++ src/components/CategoryListPage.tsx
@@ -27,9 +27,7 @@ function CategoryListPage() {
   const getCategory = async() => {
     try {
       const token = await user.getIdToken();
-      console.log("token:", token);
-      console.log("uuid:", uuid);
-      const response = await getApi(`https://anki-support-backend.onrender.com/get-category?uuid=${encodeURIComponent(uuid)}`, token);
+      const response = await getApi(`${process.env.REACT_APP_API_GET_CATEGORY_URL}?uuid=${encodeURIComponent(uuid)}`, token);
       setQuestions(response);
     } catch(e) {
       alert("カテゴリーの取得に失敗しました。");
